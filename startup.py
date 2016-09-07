1d4a3f5b63aa5cbef3f4af8adbfe407ea05f1cf4
Kien Nguyen <kien.nguyen.the@playhousedigital.com>
2016-09-07 21:41:37 +0700

    Update logic

---
+++ startup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import sys
+import subprocess
+import optparse
+import string
+from datetime import datetime
+
+class UI:
+	# Code
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    # Color
+    ORANGE = '\033[33m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    LIGHTGRAY = '\033[97m'
+    BLACK = '\033[98m'
+
+    def echo(self) :
+    	print 'echo'
+
+class Main :
+
+	def __init__(self) :
+		param =  raw_input(UI.GREEN + 'Do you want to continue (y/n) ? ' + UI.ENDC)
+		if param == 'y' :
+			startup = Startup()
+			startup.run()
+		else :
+			print UI.RED + 'Exit.' + UI.ENDC
+			return
+
+class Startup :
+
+	tasks = [];
+
+	def init(self) :
+		tasks = [
+			['Kingliving Info', '~/Desktop/deep.md', 'subl', '&']
+		]
+		self.tasks = tasks
+	
+	def title(self) :
+		print UI.GREEN + 'Startup Helper ' + UI.BLUE +  '@author ducdh' + '' + UI.ENDC
+	
+	def run(self) :
+		self.init()
+		self.title()		
+		self.runTasks()		
+
+	def runTasks(self) :
+		tasks = self.tasks
+		for name, path, program, option in tasks :
+			print UI.GREEN + 'Open ' + name + UI.ENDC
+			command = program + ' ' + path
+			subprocess.call(command, shell=True)									
+
+main = Main()	
